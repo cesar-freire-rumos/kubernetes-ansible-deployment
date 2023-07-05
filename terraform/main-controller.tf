e25f067a7a90a67a305e7bcd167171a9e511ff1f
Cesar Freire <cfreire@cfreire.com.pt>
2023-07-05 17:23:17 +0100

    Try main controller

--- terraform/main-controller.tf
+++ terraform/main-controller.tf
@@ -4,6 +4,43 @@ provider "aws" {
   secret_key = var.aws_secret_key
 }
 
+resource "aws_security_group" "instance_ports" {
+  name        = "allow_instance_ports"
+  description = "Allow instance frontend ports"
+  ingress = [
+    {
+      description      = "HTTPS"
+      from_port        = 443
+      to_port          = 443
+      protocol         = "tcp"
+      cidr_blocks      = ["0.0.0.0/0"]
+      ipv6_cidr_blocks = ["::/0"]
+      self             = false
+      security_groups  = []
+      prefix_list_ids  = []
+    },
+    {
+      description      = "HTTP"
+      from_port        = 80
+      to_port          = 80
+      protocol         = "tcp"
+      cidr_blocks      = ["0.0.0.0/0"]
+      ipv6_cidr_blocks = ["::/0"]
+      self             = false
+      security_groups  = []
+      prefix_list_ids  = []
+    }
+  ]
+  tags = {
+    Environment = "Test"
+    Terraform   = "true"
+    Name        = "allow_instance_ports"
+    Project     = "Ansible conference"
+    Entity      = "RUMOS"
+  }
+}
+
+
 resource "aws_instance" "k8s-controller" {
   instance_type = var.instance_type
   ami           = data.aws_ami.ubuntu.id
@@ -41,41 +78,6 @@ resource "aws_instance" "k8s-controller" {
   }
 }
 
-resource "aws_security_group" "instance_ports" {
-  name        = "allow_instance_ports"
-  description = "Allow instance frontend ports"
-  ingress = [
-    {
-      description      = "HTTPS"
-      from_port        = 443
-      to_port          = 443
-      protocol         = "tcp"
-      cidr_blocks      = ["0.0.0.0/0"]
-      ipv6_cidr_blocks = ["::/0"]
-      self             = false
-      security_groups  = []
-      prefix_list_ids  = []
-    },
-    {
-      description      = "HTTP"
-      from_port        = 80
-      to_port          = 80
-      protocol         = "tcp"
-      cidr_blocks      = ["0.0.0.0/0"]
-      ipv6_cidr_blocks = ["::/0"]
-      self             = false
-      security_groups  = []
-      prefix_list_ids  = []
-    }
-  ]
-  tags = {
-    Environment = "Test"
-    Terraform   = "true"
-    Name        = "allow_instance_ports"
-    Project     = "Ansible conference"
-    Entity      = "RUMOS"
-  }
-}
 
 
 
